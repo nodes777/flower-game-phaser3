f1e91c15b1307a2cc4c3e0b03ab8ec0bec8e00d1
Taylor <taylor.nodell@gmail.com>
2019-04-19 13:30:51 -0400

    loads first flower

--- src/phaser/create.js
+++ src/phaser/create.js
@@ -1,12 +1,12 @@
 export function create() {
-	const logo = this.add.image(400, 150, "logo");
+	const firstFlower = this.add.image(400, 150, "blankFlower");
 
-	this.tweens.add({
-		targets: logo,
-		y: 450,
-		duration: 2000,
-		ease: "Power2",
-		yoyo: true,
-		loop: -1
-	});
+	// this.tweens.add({
+	// 	targets: logo,
+	// 	y: 450,
+	// 	duration: 2000,
+	// 	ease: "Power2",
+	// 	yoyo: true,
+	// 	loop: -1
+	// });
 }
--- src/phaser/preload.js
+++ src/phaser/preload.js
@@ -1,5 +1,71 @@
 import logoImg from "../assets/logo.png";
-
+import blankFlower from "../assets/blank-flower.png";
 export function preload() {
 	this.load.image("logo", logoImg);
+	this.load.image("blankFlower", blankFlower);
+
+	// display progress bar
+	const progressBar = this.add.graphics();
+	const progressBox = this.add.graphics();
+	progressBox.fillStyle(0x222222, 0.8);
+	progressBox.fillRect(240, 270, 320, 50);
+
+	const width = this.cameras.main.width;
+	const height = this.cameras.main.height;
+	const loadingText = this.make.text({
+		x: width / 2,
+		y: height / 2 - 50,
+		text: "Loading...",
+		style: {
+			font: "20px monospace",
+			fill: "#ffffff"
+		}
+	});
+	loadingText.setOrigin(0.5, 0.5);
+
+	const percentText = this.make.text({
+		x: width / 2,
+		y: height / 2 - 5,
+		text: "0%",
+		style: {
+			font: "18px monospace",
+			fill: "#ffffff"
+		}
+	});
+	percentText.setOrigin(0.5, 0.5);
+
+	const assetText = this.make.text({
+		x: width / 2,
+		y: height / 2 + 50,
+		text: "",
+		style: {
+			font: "18px monospace",
+			fill: "#ffffff"
+		}
+	});
+	assetText.setOrigin(0.5, 0.5);
+
+	// update progress bar
+	this.load.on("progress", function(value) {
+		percentText.setText(parseInt(value * 100) + "%");
+		progressBar.clear();
+		progressBar.fillStyle(0xffffff, 1);
+		progressBar.fillRect(250, 280, 300 * value, 30);
+	});
+
+	// update file progress text
+	this.load.on("fileprogress", function(file) {
+		assetText.setText("Loading asset: " + file.key);
+	});
+
+	// remove progress bar when complete
+	this.load.on("complete", function() {
+		progressBar.destroy();
+		progressBox.destroy();
+		loadingText.destroy();
+		percentText.destroy();
+		assetText.destroy();
+	});
+
+	// load assets needed in our game
 }
