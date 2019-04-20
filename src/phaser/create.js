3fac8b1d95e1454449bd6708d93f478c4a589801
Taylor <taylor.nodell@gmail.com>
2019-04-20 08:59:24 -0400

    gets bee to head towards flower

--- src/phaser/create.js
+++ src/phaser/create.js
@@ -1,7 +1,7 @@
 export function create() {
 	this.firstFlower = this.physics.add.sprite(400, 150, "blankFlower");
 	this.bee = this.physics.add.sprite(200, 150, "bee");
-
+	this.secondFlower = this.physics.add.sprite(600, 250, "blankFlower");
 	// Set image/sprite properties
 	//background.setOrigin(0.5, 0.5).setDisplaySize(1600, 1200);
 	this.bee
--- src/phaser/update.js
+++ src/phaser/update.js
@@ -1,8 +1,25 @@
 export function update() {
-	this.bee.rotation = Phaser.Math.Angle.Between(
-		this.bee.x,
-		this.bee.y,
-		this.firstFlower.x,
-		this.firstFlower.y
+	// this.bee.rotation = Phaser.Math.Angle.Between(
+	// 	this.bee.x,
+	// 	this.bee.y,
+	// 	this.firstFlower.x,
+	// 	this.firstFlower.y
+	// );
+
+	// Move at 200 px/s:
+	this.physics.accelerateToObject(this.bee, this.firstFlower, 60, 300, 300);
+
+	// collider for collision with bee and flower
+	let collider = this.physics.add.overlap(
+		this.bee,
+		this.firstFlower,
+		function(beeOnFlower) {
+			//Stop moving the bee
+			this.bee.body.setAcceleration(0);
+			this.physics.world.removeCollider(collider);
+			// find a new flower and set as destination
+		},
+		null,
+		this
 	);
 }
