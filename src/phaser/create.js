1cd70cb5d53b282b7932dfb5496cdf49b929e2e1
Taylor <taylor.nodell@gmail.com>
2019-04-24 17:08:53 -0400

    about to add react components and store

--- src/phaser/create.js
+++ src/phaser/create.js
@@ -1,11 +1,33 @@
 export function create() {
 	this.firstFlower = this.physics.add.sprite(400, 150, "blankFlower");
 	this.bee = this.physics.add.sprite(200, 150, "bee");
-	this.secondFlower = this.physics.add.sprite(600, 250, "blankFlower");
+	this.secondFlower = this.physics.add.sprite(
+		Phaser.Math.RND.integerInRange(25, 600),
+		Phaser.Math.RND.integerInRange(25, 600),
+		"blankFlower"
+	);
 	// Set image/sprite properties
 	//background.setOrigin(0.5, 0.5).setDisplaySize(1600, 1200);
 	this.bee
 		.setOrigin(0.5, 0.5)
-		.setCollideWorldBounds(true)
+		//.setCollideWorldBounds(true)
 		.setDrag(500, 500);
+
+	// Move at 200 px/s:
+	this.physics.accelerateToObject(this.bee, this.firstFlower, 60, 300, 300);
+
+	// collider for collision with bee and flower
+	let collider = this.physics.add.overlap(
+		this.bee,
+		this.firstFlower,
+		function(beeOnFlower) {
+			console.log("collision");
+			//Stop moving the bee
+			this.bee.body.setAcceleration(0);
+			this.physics.world.removeCollider(collider);
+			// find a new flower and set as destination
+		},
+		null,
+		this
+	);
 }
--- src/phaser/update.js
+++ src/phaser/update.js
@@ -5,21 +5,9 @@ export function update() {
 	// 	this.firstFlower.x,
 	// 	this.firstFlower.y
 	// );
-
-	// Move at 200 px/s:
-	this.physics.accelerateToObject(this.bee, this.firstFlower, 60, 300, 300);
-
-	// collider for collision with bee and flower
-	let collider = this.physics.add.overlap(
-		this.bee,
-		this.firstFlower,
-		function(beeOnFlower) {
-			//Stop moving the bee
-			this.bee.body.setAcceleration(0);
-			this.physics.world.removeCollider(collider);
-			// find a new flower and set as destination
-		},
-		null,
-		this
-	);
+	// check for new flowers
+	// if new flowers, add them to the scene
+	// 		add new flowers for bee fly to
+	// if bee collided with current target
+	// 		set target to new flower
 }
