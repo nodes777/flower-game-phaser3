f6ddb57f4c23db990cb2e3a3b851934edb3728ac
Taylor <taylor.nodell@gmail.com>
2019-04-26 09:47:32 -0400

    sets tint color from genotype

--- src/utils/checkNewFlowers.js
+++ src/utils/checkNewFlowers.js
@@ -1,3 +1,4 @@
+import { colors } from "./colors";
 export function checkNewFlowers(
 	numFlowersPrev,
 	numFlowersCurr,
@@ -11,10 +12,27 @@ export function checkNewFlowers(
 		// for each of the new flowers in state, add them to the game
 		for (let i = numFlowersPrev; i < currState.flowers.allIds.length; i++) {
 			let currFlowerId = currState.flowers.allIds[i];
-			game.physics.add.sprite(
+			let newFlowerSprite = game.physics.add.sprite(
 				currState.flowers.byId[currFlowerId].position.x,
 				currState.flowers.byId[currFlowerId].position.y,
 				"blankFlower"
+			);
+			console.log(
+				colors[
+					currState.flowers.byId[currFlowerId].genotype.color[
+						Math.floor(Math.random())
+					]
+				]
+			);
+			// set the color by picking a random color in the flowers genotype array
+			// prefix with 0x to expect hex
+			newFlowerSprite.setTint(
+				"0x" +
+					colors[
+						currState.flowers.byId[currFlowerId].genotype.color[
+							Math.floor(Math.random())
+						]
+					]
 			);
 		}
 	}
