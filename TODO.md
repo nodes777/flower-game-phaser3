863dfb60970449cd7c24d958d82bf61496678ba6
Taylor <taylor.nodell@gmail.com>
2021-01-20 15:42:04 +1030

    updates about screen

--- TODO.md
+++ TODO.md
@@ -1,45 +1,39 @@
 # TO DO
 
--   Set available tiles to be a formula
+- Set available tiles to be a formula
 
 ## Tests
 
--   /components and /phaser
+- /components and /phaser
 
 ## Phaser
 
--   Multiple bees
--   The bee's movement is a hack that slows down performance
--   On mouse hover display name (perf issues)
+- Multiple bees
+- The bee's movement is a hack that slows down performance
+- On mouse hover display name (perf issues)
 
 ### Art
 
--   Add new stems
+- Add new stems
 
 ## Redux
 
 ## React
 
--   Make config panel pretty
--   Create a table to display what's recessive (italices on FlowerTable and within the select for recessive)
--   Reuse drop down custom select structure for add recessive trait select?
--   Order the flower table genes to show the phenotype allele first, or only phenotype
-
 ## Features
 
--   Determine size of garden
--   Fractals or math formulas to determine garden shape
--   Add growth stages
--   Save to url to share somehow
--   Changing the Punnett Table changes the game flowers
--   Allow running in the background
+- Determine size of garden
+- Fractals or math formulas to determine garden shape
+- Add growth stages
+- Save to url to share somehow
+- Allow running in the background
 
 ## A11y
 
 ## Minor
 
--   Add Favico
+- Add Favico
 
 ## Bugs
 
--   The bee can get stuck orbitting flowers
+- The bee can get stuck orbitting flowers
--- src/components/BottomContainer/AboutScreen.js
+++ src/components/BottomContainer/AboutScreen.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
-
+import "../../css/about.css";
+const url = "https://en.wikipedia.org/wiki/Punnett_square";
 class AboutScreen extends Component {
 	render() {
 		return (
 			<div className="punnett" role="region" aria-label={`About Screen`}>
 				<h1>About</h1>
 				<p>
-					This is a flower gene simulation. Pick a
-					<a href="https://en.wikipedia.org/wiki/Punnett_square">
-						punnett square
-					</a>
-					configuration for color and shape of the two inital parents, then
-					activate the Bee play button to start the simulation. You can set a
-					gene as recessive in the options screen. The bee will not pollinate
-					the same flower (a single flower cannot be both parents), but the
-					flowers do not have sexes.
+					This is a flower gene simulation. Pick a{" "}
+					<a href={url}>punnett square</a> configuration for color and shape of
+					the two inital parents, then activate the Bee play button to start the
+					simulation. You can set a gene as recessive in the options screen. The
+					bee will not pollinate the same flower (a single flower cannot be both
+					parents), but the flowers do not have sexes.
 				</p>
-				<a href="https://github.com/nodes777/flower-game-phaser3">
-					Github Repo
-				</a>
+				<p>
+					<a href="https://github.com/nodes777/flower-game-phaser3">
+						Github Repo
+					</a>
+				</p>
 			</div>
 		);
 	}
