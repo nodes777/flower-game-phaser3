dce20e03f698a0662de54e052fbe598231e7acbc
Taylor <taylor.nodell@gmail.com>
2021-01-21 12:03:49 +1030

    puts beeCanFly code earlier

--- src/phaser/scenes/create.js
+++ src/phaser/scenes/create.js
@@ -5,7 +5,7 @@ export function create() {
 	// width and height from image properties
 	this.background = this.add.tileSprite(0, 0, 1280, 800, "background");
 	this.background.setScale(2.6).setOrigin(0).setDepth(-13); // first tile's depth is -12.5
-
+	this.beeCanFly = getStoreState().config.beeCanFly;
 	// builds garden and creates initial flowers
 	init(this);
 
@@ -20,9 +20,8 @@ export function create() {
 		loop: true,
 	});
 
-	this.beeCanFly = getStoreState().config.beeCanFly;
 	// keep track of which flowers are "growing"/ moving upward
-	this.flowersGrowing = [];
+	//this.flowersGrowing = [];
 
 	// hack to reset bee's accelleration every 1/10 sec so that orbits appear less frequently
 	// this.time.addEvent({
